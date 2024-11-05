32e976a49735dbee73c1d271dd07b2624d948fd1
Selena <selena.oerding@gmail.com>
2024-11-05 13:47:14 +0100

    final

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,16 +1,10 @@
+///_______________Med HTML user interface__________________
+
 // Modeling the menu
 const vegetarian = "Vegetarian Pizza";
 const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
 const pizzaPrice = 80;
-
-// Greeting the customer
-alert(
-  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`
-);
-
-// Ask user which pizza
-const orderName = promt("Enter the name of the pizza you want to order today:");
 
 // Check if pizza is on the menu
 function checkOrderName(orderName) {
@@ -25,34 +19,117 @@ function checkOrderName(orderName) {
   }
 }
 
-// How many pizzas?
-if (!checkOrderName(orderName)) {
-  alert("Sorry, that pizza is not on our menu!");
-} else {
-  const orderQuantity = prompt(`How many of ${orderName} do you want?`);
-}
+function handleSubmit(event) {
+  event.preventDefault();
 
-// Calculate how many pizzas.
-function totalCost(orderQuantity) {
-  return orderQuantity * pizzaPrice;
-}
+  // Get values from form instead of prompt
+  const orderName = document.getElementById("pizzaType").value;
+  const orderQuantity = parseInt(document.getElementById("quantity").value);
+  const resultDiv = document.getElementById("result");
+  resultDiv.style.display = "block";
 
-const cost = totalCost(orderQuantity);
-
-// Calculate cooking time.
-function cookingTime(orderQuantity) {
-  if (orderQuantity >= 6) {
-    return 20;
-  } else if (orderQuantity >= 3) {
-    return 15;
-  } else {
-    return 10;
+  // Check pizza and show error in resultDiv instead of alert
+  if (!checkOrderName(orderName)) {
+    resultDiv.innerHTML = "Sorry, that pizza is not on our menu!";
+    return false;
   }
+
+  // Validate number and calculate cost
+  if (isNaN(orderQuantity) || orderQuantity <= 0) {
+    resultDiv.innerHTML = "Please enter a valid number of pizzas.";
+    return false;
+  }
+
+  function totalCost(orderQuantity) {
+    return orderQuantity * pizzaPrice;
+  }
+
+  const cost = totalCost(orderQuantity);
+
+  // Calculate cooking time
+  function cookingTime(orderQuantity) {
+    if (orderQuantity >= 6) {
+      return 20;
+    } else if (orderQuantity >= 3) {
+      return 15;
+    } else {
+      return 10;
+    }
+  }
+
+  const time = cookingTime(orderQuantity);
+
+  // Show final message in resultDiv instead of alert
+  resultDiv.innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${cost} kr. The pizzas will take ${time} minutes.`;
+
+  return false;
 }
 
-const time = cookingTime(orderQuantity);
+// Add form listener
+document.getElementById("pizzaForm").addEventListener("submit", handleSubmit);
 
-// Final message
-alert(
-  `Great, I'll get started on your ${orderName} right away, it will cost ${cost} kr. The pizzas will take ${time} minutes. `
-);
+///_______________Orginal__________________
+
+// // Modeling the menu
+// const vegetarian = "Vegetarian Pizza";
+// const hawaiian = "Hawaiian Pizza";
+// const pepperoni = "Pepperoni Pizza";
+// const pizzaPrice = 80;
+
+// // Greeting the customer
+// alert(
+//   `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`
+// );
+
+// // Ask user which pizza
+// const orderName = prompt(
+//   "Enter the name of the pizza you want to order today:"
+// );
+
+// // Check if pizza is on the menu
+// function checkOrderName(orderName) {
+//   if (
+//     orderName === vegetarian ||
+//     orderName === hawaiian ||
+//     orderName === pepperoni
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// // How many pizzas?
+// const orderQuantity = parseInt(prompt(`How many of ${orderName} do you want?`));
+// if (!checkOrderName(orderName)) {
+//   alert("Sorry, that pizza is not on our menu!");
+// }
+
+// // Make sure that a valid number is given and caculate the total price.
+// if (isNaN(orderQuantity) || orderQuantity <= 0) {
+//   alert("Please enter a valid number of pizzas.");
+// } else {
+//   function totalCost(orderQuantity) {
+//     return orderQuantity * pizzaPrice;
+//   }
+// }
+
+// const cost = totalCost(orderQuantity);
+
+// // Calculate cooking time.
+// function cookingTime(orderQuantity) {
+//   if (orderQuantity >= 6) {
+//     return 20;
+//   } else if (orderQuantity >= 3) {
+//     return 15;
+//   } else {
+//     return 10;
+//   }
+// }
+
+// const time = cookingTime(orderQuantity);
+
+// // Final message
+// alert(
+//   `Great, I'll get started on your ${orderName} right away, it will cost ${cost} kr. The pizzas will take ${time} minutes. `
+// );
